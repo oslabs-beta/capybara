9dde0a126bcf03410fbfc5cbcce57f32ac03a6e4
Wenjun Song <wenjunsong@Wenjuns-MacBook-Air.local>
2025-04-29 00:52:50 -0400

    store cache and query cache

--- server/src/services/processEvent.ts
+++ server/src/services/processEvent.ts
@@ -5,25 +5,51 @@
 
 import connectRedis from '../utils/redisClient';
 import chalk from 'chalk';
+import { v4 as uuidv4 } from 'uuid';
 
+const TTL = 7 * 24 * 60 * 60; // Set expiration after 7 day
+const maxLength = 1000; // Save amount of records up to 1000
+
+// ------------------------------------------------------------------------------------------------
+// * Store Error Events with TTL
+// ------------------------------------------------------------------------------------------------
 const redisCache = async (topicName: string, event: Record<string, any>) => {
   try {
     const redis = await connectRedis();
-    const redisKey = `${topicName}`; // Create redis key
 
-    // Store error events into Redis List
-    const errorEvent = await redis.rPush(redisKey, JSON.stringify(event));
-    console.log(chalk.green(`[Redis] Event had been saved as ${errorEvent}.`));
+    const redisKey = `${topicName}:${uuidv4()}`; // Create unique error key
 
-    // Read all elements from the List
-    const events = await redis.lRange(redisKey, 0, -1);
-    console.group(chalk.bgMagenta(`[Redis] List: ${events}`))
+    await redis.set(redisKey, JSON.stringify(event), { EX: TTL });
 
-    // TODO: SET TTL & BATCH QUERY
-    
+    const listKey = `${topicName}`; // Initialize list key
+    await redis.lPush(listKey, redisKey); // Save error keys into list
+
+    const currentLength = await redis.lLen(listKey); // Current length of Redis list
+
+    if (currentLength > maxLength) {
+        await redis.lTrim(listKey, 0, maxLength - 1); // Resize the length of list
+    }
+
+    console.log(chalk.green(`[Redis] Event had been saved under key ${redisKey}.`));
   } catch (error) {
     console.error(chalk.redBright('[Redis] Error storing event: '), error);
   }
 };
+
+// ------------------------------------------------------------------------------------------------
+// * Query Error Events with Batching
+// ------------------------------------------------------------------------------------------------
+const queryCache = async(topicName: string, amount=10) => {
+    const redis = await connectRedis();
+
+    const listKey = `${topicName}`; // Obtain list key
+    const errorKeys = await redis.lRange(listKey, 0, amount - 1); // Error event keys
+
+    // Get the results in batch
+    const events = await redis.mGet(errorKeys);
+    
+    // ? FILTERING
+    return events;
+}
 
 export default redisCache;
