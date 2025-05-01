61e0f4ba895648467b401ddc13902482905e7c67
Wenjun Song <wenjunsong@Wenjuns-MacBook-Air.local>
2025-04-30 22:51:31 -0400

    fix redis cache server

--- server/src/utils/redisClient.ts
+++ server/src/utils/redisClient.ts
@@ -2,22 +2,27 @@ import { createClient } from 'redis';
 import chalk, { Chalk } from 'chalk';
 import getSecretKeys from '../appSecrets';
 
+// Initialize redisClient to null 
+let redisClient: ReturnType<typeof createClient> | null = null;
+
 const connectRedis = async () => {
+  if (redisClient && redisClient.isOpen) return redisClient;
+  console.log(chalk.bgYellowBright(`[REDIS] CONNECTION PORT EXISTING`));
+
   const { REDIS_URL } = await getSecretKeys(); // Get saved URL from secrete manager
 
   if (!REDIS_URL) {
     throw new Error('REDIS_URL is not defined in secrets');
   }
 
-  const redisClient = createClient({ url: REDIS_URL }); 
+  redisClient = createClient({ url: REDIS_URL }); 
 
-  // Listen for connection error event asynchronously
+
   redisClient.on('error', (err) => {
-    console.error(chalk.green('Redis Client Error', err));
+    console.error(chalk.bgRedBright('[Redis] Client ', err));
   });
 
   await redisClient.connect();
-  console.log(chalk.green('[Redis] You are connected to Redis Cloud now.'));
 
   return redisClient;
 };
