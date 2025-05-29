2b670482b5d2e4be4dbfd26a55d838e62ba29932
Wenjun Song <wenjunsong@Wenjuns-MacBook-Air.local>
2025-05-29 18:40:52 -0400

    line chart test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,28 @@
 // ----------------------------------------------------------
 // >> APP COMPONENT << //
 // ----------------------------------------------------------
-import TestComponent from './components/TestComponent';
+import { Container, Typography } from '@mui/material';
+import Grid from '@mui/material/Grid';
+
+// import TestComponent from './components/TestComponent';
+import {CpuChart} from './components/GcpMetric';
 
 const App = () => {
   // ----------------------------------------------------------
   // >> COMPONENT RENDERING << //
   // ----------------------------------------------------------
   return (
-    <>
-      <TestComponent />
-    </>
-  );
+    <Container maxWidth="md">
+      <Typography variant="h4" gutterBottom>
+        GCP Metrics Dashboard
+      </Typography>
+      <Grid container spacing={2}>
+        <Grid>
+          <CpuChart />
+        </Grid>
+      </Grid>
+    </Container>
+  )
 };
 
 export default App;
