685b342e2c0fdf71d7c89bbd1d9c715bdd4f6ba1
Amit <149993488+user0824@users.noreply.github.com>
2025-05-31 17:58:02 -0400

    build: fixed tests

--- client/src/components/TestComponent.tsx
+++ client/src/components/TestComponent.tsx
@@ -4,7 +4,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Typewriter } from 'react-simple-typewriter';
-import { Button } from '@/components/ui/button';
 
 const TestComponent: React.FC = () => {
   // Add notification when frontend connects to server
