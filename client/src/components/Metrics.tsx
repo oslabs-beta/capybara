96d216c46ae0b0b7bd8f5bf3e96facabe3167a81
Amit <149993488+user0824@users.noreply.github.com>
2025-06-03 02:28:34 -0400

    feat: dashboard updates. Updated metrics component.

--- client/src/components/Metrics.tsx
+++ client/src/components/Metrics.tsx
@@ -1,29 +1,28 @@
 // ----------------------------------------------------------
 // >> METRICS << //
 // ----------------------------------------------------------
-import BarChartExample from './ExampleBarChart';
-
-import InteractiveLineChartExample from './ExampleInteractiveLineChart';
-import InteractiveAreaChartExample from './ExampleInteractiveAreaChart';
-import LineChartExample from './ExampleLineChart';
+// import BarChartExample from './ExampleBarChart';
+// import InteractiveLineChartExample from './ExampleInteractiveLineChart';
+// import InteractiveAreaChartExample from './ExampleInteractiveAreaChart';
+// import LineChartExample from './ExampleLineChart';
 import CpuMemoryUtilization from './CpuRequestUtilization';
 import RecentUsage from './RecentUsage';
 // import EventLog from './EventLog';
-import RadialChartExample from './ExampleRadialChart';
+// import RadialChartExample from './ExampleRadialChart';
 
 const Metrics: React.FC = () => {
   return (
     <div>
       <RecentUsage />
       <CpuMemoryUtilization />
-      <h1>~~~~~~~~~~ Metrics TEST COMPONENTS START ~~~~~~~~~~</h1>
+      {/* <h1>~~~~~~~~~~ Metrics TEST COMPONENTS START ~~~~~~~~~~</h1> */}
       {/* <EventLog /> */}
       {/* <BarChartExample />
       <InteractiveAreaChartExample />
       <RadialChartExample />
       <InteractiveLineChartExample />
       <LineChartExample /> */}
-      <h1>~~~~~~~~~~ Metrics TEST COMPONENTS END ~~~~~~~~~~</h1>
+      {/* <h1>~~~~~~~~~~ Metrics TEST COMPONENTS END ~~~~~~~~~~</h1> */}
     </div>
   );
 };
